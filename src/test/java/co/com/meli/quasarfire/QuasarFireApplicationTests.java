d5d27bc532cdd9632a4aa33df596c3e355834219
Jhon Eduar Cuellar Ballen <jhon.cuellarb@claro.com.co>
2021-02-12 01:11:06 -0500

    Proyecto Fuego de Quasar

--- src/test/java/co/com/meli/quasarfire/QuasarFireApplicationTests.java
+++ src/test/java/co/com/meli/quasarfire/QuasarFireApplicationTests.java
@@ -1,13 +1,21 @@
 package co.com.meli.quasarfire;
 
+import co.com.meli.quasarfire.controller.QuasarFireController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class QuasarFireApplicationTests {
 
+	@Autowired
+	private QuasarFireController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
